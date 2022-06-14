df3f3389e2a312b3d255453e61f3ffb2283b351f
davidzliu108 <davidzliu@gmail.com>
2022-06-14 16:27:28 -0400

    navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">Film Select</a>
+        <ul>
+            <li>Home</li>
+            <li>Selectr</li>
+        </ul>
+    </nav>
+}
