81b9afad9422ff14b2aaae6b13ad46d7b43efa7e
sliu-rv <sliu@redventures.com>
2022-06-15 14:39:47 -0400

    added cards in search

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Card from "react-bootstrap/Card";
+import { Button } from 'react-bootstrap';
 
 const MovieList = (props) => {
 
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-				</div>
+                <div class="card" style={{ width: '18rem' }}>
+                    <img class="card-img-top searchImg" variant="top" src={movie.Poster} />
+                    <Card.Body>
+                        <Card.Title>{movie.Title}</Card.Title>
+                        <Card.Text>
+                        Year: {movie.Year}
+                        </Card.Text>
+                        <Button variant="primary">N</Button>
+                        <Button variant="primary">D</Button>
+                        <Button variant="primary">H</Button>
+                    </Card.Body>
+                </div>               
 			))}
 		</>
 	);
