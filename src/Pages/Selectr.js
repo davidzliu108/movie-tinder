dcfc265af3fcdea40b65e4a7a5ff9339ac142c6d
davidzliu108 <davidzliu@gmail.com>
2022-06-15 13:08:13 -0400

    movietinder not working

--- src/Pages/Selectr.js
+++ src/Pages/Selectr.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import { Card } from 'react-bootstrap';
 
 const Selectr = () => {
+    const [randomId, setRandomId] = useState('tt0076759');
+    const [film, setFilm] = useState('');
+
+    const getRandomFilm = async (randomId) => {
+        const url = "http://www.omdbapi.com/?i=${randomId}&apikey=fd314c78"
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        setFilm(responseJson.Title);
+    }
+
+    useEffect(() => {
+        
+        getRandomFilm('tt0076759');
+    })
+
+
     return (
-        <div>
-            <p>bruh</p>
-        </div>
+        <Container>
+            <h2>Recommendations</h2>
+            <Card>
+                {film}
+            </Card>
+        </Container>
     );
 }
 
