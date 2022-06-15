bbdfc209509fb806da94d53189bec4d39129a6f2
sliu-rv <sliu@redventures.com>
2022-06-15 16:16:28 -0400

    nicer search

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -10,17 +10,20 @@ const MovieList = (props) => {
                 <div class="card" style={{ width: '18rem', margin: '1rem' }}>
                     <img class="card-img-top searchImg" variant="top" src={movie.Poster} />
                     <Card.Body>
-                        <Card.Title>{movie.Title} {movie.Year} </Card.Title>
-                        <Button class="mr-1" variant="link">
+                        <Card.Title>{movie.Title} {'(' + movie.Year + ')'} </Card.Title>
+                    </Card.Body>
+                    <div class = 'card-footer mx-auto'>
+                        <Button class="mr-1 searchButton" variant="link">
                             <img src="./netflix.png" height="30" width="30"></img>
                         </Button>
-                        <Button class="mr-1" variant="link">
+                        <Button class="mr-1 searchButton" variant="link">
                             <img src="./disney.png" height="30" width="30"></img>
                         </Button>
-                        <Button class="mr-1" variant="link">
+                        <Button class="mr-1 searchButton" variant="link">
                             <img src="./hbomax.png" height="30" width="30"></img>
                         </Button>
-                    </Card.Body>
+                    </div>
+                    
                 </div>               
 			))}
 		</>
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const SearchBox = (props) => {
 	return (
-		<div className='col col-sm-4'>
+		<div className='col bar'>
 			<input
+				class="searchBox"
 				className='form-control'
 				value={props.value}
 				onChange={(event) => props.setSearchValue(event.target.value)}
